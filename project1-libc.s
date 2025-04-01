64452c9145325c89f71fcee8ef0e10cf121b0416
jacksoncolorado <jackson.broncos@gmail.com>
2025-04-01 12:36:08 -0600

    Update project1-libc.s
    updated gets

--- project1-libc.s
+++ project1-libc.s
@@ -77,42 +77,64 @@ putchar:
     addi sp, sp, -1
     sb a0, 0(sp)	# push a0
 
+    addi sp, sp, -16
+    sw ra, 12(sp)	# push ra
+    sw a1, 8(sp)	# push a1
+    sw a2, 4(sp)	# push a2
+    sw a7, 0(sp)	# push a7
+
+
     li a0, STDOUT	# Put STDOUT code into a0
     la a1, sp	# Put sp into a1 // pointer to input char
     li a2, 1	# Put value 1 into a2 // write 1 byte
     li a7, __NR_WRITE	# Put NR_WRITE code into a7
     ecall
 
-
     // epilog
+    sw a7, 0(sp)	# push a7
+    sw a2, 4(sp)	# push a2
+    sw a1, 8(sp)	# push a1
+    sw ra, 12(sp)	# push ra
+    addi sp, sp, -16
+
     lbu a0, 0(a1)	# load ( byte value ) of * sp back into a0 ( using lbu )
-    lb a0, 0(sp)	# push a0
     addi sp, sp, 1	# restore sp
 
 ret
 
 gets:
-# Prolog : a0 contains addr pointer s
-# make room for 3 items on the stack
-# save ra , a0 ( original s pointer )
-    mv t0, a0
-# Body
-# move a0 into t0 ( use t0 for local pointer p )
-gets_loop :
-// do - while body
-# save t0 into stack ( local p pointer )
+// Prolog
+addi sp, sp, -12 # make room for 3 items on the stack
+sw ra, 8(sp)	# save ra 
+sw a0, 4(sp)	# a0 ( original s pointer )
 
-    loop2:
-        call getchar
-        sb a0, 0(t0)
-        beq a0, '\n', finish2
-        addi, t0, t0 , 1
-        j loop2
-    
-    finish2:
-        sb zero, 0(t0)
-        mv a0, t0
-        ret
+// Body
+mv t0, a0	# move a0 into t0 ( use t0 for local pointer p )
+
+gets_loop:
+    // do-while body
+    sw t0, 0(sp)	# save t0 into stack ( local p pointer )
+    call getchar	# Call getchar
+    lw t0, 0(sp)	# Restore t0 from stack, At this point , the read-in char put into a0
+    blt a0, zero, gets_exit	# If read - in char is negative , goto gets_exit
+    sb a0, 0(t0)
+    addi t0, t0, 1	# Increment t0 by 1
+
+    // Condition check ( do-while )
+    li t1, 10	# put newline char into an unused register ( ascii value 10)
+    bne a0, t1, gets_loop	# goto gets_loop if read - in char is not newline
+
+    // If was newline
+    addi t0, t0, -1	# end the input string with a terminating 0 byte ( note that this will overwrite the ‘\n ’ with NULL char )
+    sb zero, 0(t0)
+    lw a0, 4(sp)	# Restore base address s into register a0
+    sub a0, t0, a0 	# Put t0 - a0 into a0 // return length of read - in string (p - s )
+
+gets_exit:
+    lw ra, 8(sp)	# Restore return address from stack
+    addi sp, sp, 12	# restore stack
+
+ret
 
 getchar:
     la a1, temp
