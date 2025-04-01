0e86cc59bcc5956e2e489880157f9929fe0d3a19
jacksoncolorado <jackson.broncos@gmail.com>
2025-03-31 19:37:46 -0600

    Update project1-libc.s
    modified puts to deal with stack

--- project1-libc.s
+++ project1-libc.s
@@ -14,39 +14,72 @@ main:
 	# main() body
 
 	# Write the prompt to the terminal (stdout)
-	la a0, prompt
-    call puts
+	la a0, prompt	# Put string pointer ( prompt ) into a0
+        call puts	# Call puts
 
 	#  Read up to 100 characters from the terminal (stdin)
-	la a0, buffer
-	call gets
+	la a0, buffer	# Put address pointer ( buf ) into a0
+	call gets	# Call gets
 
 	# Write the just read characters to the terminal (stdout)
-	la a0, buff
-	call puts
+	la a0, buff	# Put string pointer ( buf ) into a0
+	call puts	# Call puts
 
 	# main() epilog
 	lw ra, 100(sp)
 	addi sp, sp, 104
 	ret
+halt :
+	ebreak
+	j halt 	# stop execution
 	
 puts:
+// prolog
+addi sp, sp, -12
+sw ra, 8(sp)	# push ra
+sw t0, 4(sp)	# push t0
+sw a0, 0(sp)	# push a0
+
     mv t0, a0
     
-    loop:
+    puts_loop:
         lb a0, 0(t0)
-        beqz a0, finish
+        beqz a0, puts_finish
         call putchar
+	blti a0, 0, puts_error
         addi, t0, t0, 1
-        j loop
-      
-    finish:
+        j puts_loop
+
+	# Error branch ( a0 < 0)
+    puts_error:
+    	li a0, -1	# Put -1 into a0 // return -1
+	j epil_error	# Restore stack items and sp      
+    
+    puts_finish:
         li a0, '\n'
         call putchar
         li a0, 0 
-        ret
+	
+// epilog
+lw a0, 0(sp)	# pop a0
+
+# epilogue without a0
+epil_error:
+
+	lw ra, 8(sp)	# push ra
+	lw t0, 4(sp)	# push t0
+	addi sp, sp, 12
+
+ret
 
 putchar:
+// prolog
+// push ra
+// push a7
+// push t0
+// push a1
+// push a2
+
     li a7, __NR_WRITE
     mv t0, a0
     li a0, STDOUT
@@ -56,11 +89,25 @@ putchar:
     ecall
     mv a0, t0
     ret
-    
+
+// epilog
+// push a2
+// push a1
+// push t0
+// push a7
+// push ra
 
 
 gets:
+# Prolog : a0 contains addr pointer s
+# make room for 3 items on the stack
+# save ra , a0 ( original s pointer )
     mv t0, a0
+# Body
+# move a0 into t0 ( use t0 for local pointer p )
+gets_loop :
+// do - while body
+# save t0 into stack ( local p pointer )
 
     loop2:
         call getchar
